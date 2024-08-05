640424698e3489cd6956007b007b3cc75653cd57
Nissan Yamin <nissanyam1@gmail.com>
2024-08-05 15:35:46 +0300

    Commit message describing the changes

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -1,62 +1,135 @@
-// import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import '../constants/dotenv_constants.dart';
 
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:http/http.dart' as http;
-// import 'package:reciperescue_client/models/ingredient_model.dart';
-// import '../authentication/auth.dart';
-// import '../constants/dotenv_constants.dart';
-// import 'questionnaire_controller.dart';
+// HTTP Methods
+const String HTTP_METHOD_GET = 'GET';
+const String HTTP_METHOD_POST = 'POST';
+const String HTTP_METHOD_PUT = 'PUT';
+const String HTTP_METHOD_DELETE = 'DELETE';
 
-// class HouseholdController extends GetxController {
-//   final isJoinExistingHousehold = Rx(true);
-//   var householdId = "".obs;
-//   var newHouseholdName = "".obs;
+Future<void> performHttpRequest(
+    String url, Map<String, dynamic> parameters, String method,
+    [Map<String, dynamic>? body] // Optional body parameter
+    ) async {
+  try {
+    Uri uri = Uri.parse(url).replace(queryParameters: parameters);
+    http.Response response;
 
-//   TextEditingController existingHousehold = TextEditingController();
-//   TextEditingController nameNewHousehold = TextEditingController();
+    switch (method) {
+      case HTTP_METHOD_GET:
+        response = await http.get(uri);
+        break;
+      case HTTP_METHOD_POST:
+        response = await http.post(uri, body: jsonEncode(body), headers: {
+          'Content-Type': 'application/json',
+        });
+        break;
+      case HTTP_METHOD_PUT:
+        response = await http.put(uri, body: jsonEncode(body), headers: {
+          'Content-Type': 'application/json',
+        });
+        break;
+      case HTTP_METHOD_DELETE:
+        response = await http.delete(uri, headers: {
+          'Content-Type': 'application/json',
+        });
+        break;
+      default:
+        throw Exception('Unsupported HTTP method: $method');
+    }
 
-//   void updateHouseholdName() {
-//     newHouseholdName.value = nameNewHousehold.text;
-//   }
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      print('Request successful');
+      print('Response body: ${response.body}');
+    } else {
+      print('Request failed with status: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error during HTTP request: $e');
+  }
+}
 
-//   Future<bool> createHousehold() async {
-//     try {
-//       final Uri url = Uri.parse(
-//           '${DotenvConstants.baseUrl}/users_household/createNewHousehold?user_mail=${Authenticate().currentUser!.email}&household_name=${nameNewHousehold.value.text}');
+class ProfileController extends GetxController {
+  var isEditMode = false.obs;
+  var firstName = ''.obs;
+  var lastName = ''.obs;
+  var email = ''.obs;
+  var country = ''.obs;
+  var state = ''.obs;
+  var households = <String>[].obs;
+  var userName = ''.obs;
+  var userImage = ''.obs; // Initialize as an empty string
+  var countryController = TextEditingController();
+  var stateController = TextEditingController();
+  var firstNameController = TextEditingController();
+  var lastNameController = TextEditingController();
+  var newHouseholdController = TextEditingController();
 
-//       List<Ingredient> ingredients =
-//           Get.find<QuestionnaireController>().ingredients.value;
-//       print("ingredients here: $ingredients");
-//       List<Map<String, dynamic>> householdIngredients =
-//           ingredients.map((ingredient) {
-//         return {
-//           'IngredientName': ingredient.name,
-//           'IngredientAmount': ingredient.amount ?? 1,
-//         };
-//       }).toList();
+  void toggleEditMode() {
+    isEditMode.value = !isEditMode.value;
+  }
 
-//       Map<String, dynamic> requestBody = {'ingredients': householdIngredients};
+  void setUserImage(String path) {
+    userImage.value = path;
+  }
 
-//       String requestBodyJson = jsonEncode(requestBody);
+  Future<void> saveProfile() async {
+    final url =
+        '${DotenvConstants.baseUrl}/usersAndHouseholdManagement/updatePersonalUserInfo';
+    final parameters = <String, dynamic>{};
+    final body = <String, dynamic>{
+      'first_name': firstNameController.text,
+      'last_name': lastNameController.text,
+      'email': email.value,
+      'country': countryController.text,
+      'state': stateController.text,
+    };
+    await performHttpRequest(url, parameters, HTTP_METHOD_PUT, body);
+  }
 
-//       final response = await http.post(
-//         url,
-//         headers: <String, String>{
-//           'Content-Type': 'application/json; charset=UTF-8',
-//         },
-//         body: requestBodyJson,
-//       );
+  Future<void> addHousehold(String householdId) async {
+    if (householdId.isNotEmpty) {
+      final url =
+          '${DotenvConstants.baseUrl}/usersAndHouseholdManagement/addUserToHousehold';
+      final parameters = {
+        'user_email': email.value,
+        'household_id': householdId,
+      };
+      await performHttpRequest(url, parameters, HTTP_METHOD_POST);
+      households.add(householdId);
+    }
+  }
 
-//       if (response.statusCode == 200) {
-//         print(response.body);
-//         return true;
-//       } else {
-//         return false;
-//       }
-//     } catch (error) {
-//       print('Error making POST request: $error');
-//       return false;
-//     }
-//   }
-// }
+  Future<void> removeHousehold(int index) async {
+    final householdId = households[index];
+    final url =
+        '${DotenvConstants.baseUrl}/usersAndHouseholdManagement/removeUserFromHousehold';
+    final parameters = {
+      'user_email': email.value,
+      'household_id': householdId,
+    };
+    await performHttpRequest(url, parameters, HTTP_METHOD_DELETE);
+    households.removeAt(index);
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    // Initialize with actual data if available
+    firstName.value = 'John';
+    lastName.value = 'bd616751-cc28-41b5-9719-fbf1b1f52df3';
+    email.value = 'koko@koko.com';
+    country.value = 'USA';
+    state.value = '67fc717d-67b4-43e0-a8dc-cb5189a9c383';
+    households.assignAll(
+        ['Household 1', 'Household 2', 'Household 3']); // Example households
+    firstNameController.text = firstName.value;
+    lastNameController.text = lastName.value;
+    countryController.text = country.value;
+    stateController.text = state.value;
+  }
+}
--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,20 +1,373 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
+import 'package:reciperescue_client/controllers/homepage_controller.dart';
+import 'package:reciperescue_client/controllers/profile_controller.dart';
 import 'authentication/auth.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfilePage extends StatelessWidget {
   ProfilePage({super.key});
   final Authenticate auth = Authenticate();
+  final ProfileController controller = Get.put(ProfileController());
+  final HomePageController hController = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.edit),
+            onPressed: () {
+              controller.toggleEditMode();
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Obx(() {
+          if (controller.isEditMode.value) {
+            return EditProfileForm(controller: controller);
+          } else {
+            return ViewProfile(controller: controller, auth: auth);
+          }
+        }),
+      ),
+    );
+  }
+}
+
+class EditProfileForm extends StatelessWidget {
+  final ProfileController controller;
+
+  EditProfileForm({required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ProfileImageSection(controller: controller),
+            SizedBox(height: 16),
+            ChangeImageButton(controller: controller),
+            SizedBox(height: 16),
+            PersonalInfoSection(controller: controller),
+            SizedBox(height: 16),
+            SaveButton(controller: controller),
+            SizedBox(height: 16),
+            HouseholdsSection(controller: controller),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileImageSection extends StatelessWidget {
+  final ProfileController controller;
+
+  ProfileImageSection({required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    const defaultImagePath = 'assets/images/user.png';
+
+    return Obx(() {
+      return Column(
+        children: [
+          Stack(
+            alignment: Alignment.bottomRight,
+            children: [
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: controller.userImage.value.isNotEmpty
+                    ? FileImage(File(
+                        controller.userImage.value)) // Convert String to File
+                    : AssetImage(defaultImagePath) as ImageProvider,
+              ),
+              if (controller
+                  .isEditMode.value) // Show the remove icon only in edit mode
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: IconButton(
+                    icon:
+                        Icon(Icons.remove_circle, color: Colors.red, size: 30),
+                    onPressed: () {
+                      controller.setUserImage(
+                          ""); // Set to empty string to reset to default image
+                    },
+                  ),
+                ),
+            ],
+          ),
+        ],
+      );
+    });
+  }
+}
+
+class PersonalInfoSection extends StatelessWidget {
+  final ProfileController controller;
+
+  PersonalInfoSection({required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    final firstNameLabel = 'First Name';
+    final lastNameLabel = 'Last Name';
+    final countryLabel = 'Country';
+    final stateLabel = 'State';
+    final emailLabel = 'Email';
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        if (controller.isEditMode.value) ...[
+          ShadowedTextField(
+              controller: controller.firstNameController,
+              label: firstNameLabel),
+          SizedBox(height: 16),
+          ShadowedTextField(
+              controller: controller.lastNameController, label: lastNameLabel),
+          SizedBox(height: 16),
+          ShadowedTextField(
+              controller: controller.countryController, label: countryLabel),
+          SizedBox(height: 16),
+          ShadowedTextField(
+              controller: controller.stateController, label: stateLabel),
+        ] else ...[
+          ShadowedTextBox(
+              label: firstNameLabel, value: controller.firstName.value),
+          SizedBox(height: 16),
+          ShadowedTextBox(
+              label: lastNameLabel, value: controller.lastName.value),
+          SizedBox(height: 16),
+          ShadowedTextBox(label: emailLabel, value: controller.email.value),
+          SizedBox(height: 16),
+          ShadowedTextBox(label: countryLabel, value: controller.country.value),
+          SizedBox(height: 16),
+          ShadowedTextBox(label: stateLabel, value: controller.state.value),
+        ],
+      ],
+    );
+  }
+}
+
+class HouseholdsSection extends StatelessWidget {
+  final ProfileController controller;
+
+  HouseholdsSection({required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Households:",
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 8),
+        Container(
+          height: 200,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Obx(() {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  for (var i = 0; i < controller.households.length; i++)
+                    ListTile(
+                      title: ShadowedTextBox(
+                          label: 'Household', value: controller.households[i]),
+                      trailing: controller.isEditMode.value
+                          ? IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () {
+                                controller.removeHousehold(i);
+                              },
+                            )
+                          : null,
+                    ),
+                ],
+              ),
+            );
+          }),
+        ),
+        SizedBox(height: 16),
+        if (controller.isEditMode.value) ...[
+          Row(
+            children: [
+              Expanded(
+                child: ShadowedTextField(
+                    controller: controller.newHouseholdController,
+                    label: 'New Household ID'),
+              ),
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  controller
+                      .addHousehold(controller.newHouseholdController.text);
+                  controller.newHouseholdController.clear();
+                },
+              ),
+            ],
+          ),
+        ],
+      ],
+    );
+  }
+}
+
+class ChangeImageButton extends StatelessWidget {
+  final ProfileController controller;
+
+  ChangeImageButton({required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () async {
+        final pickedFile =
+            await ImagePicker().pickImage(source: ImageSource.gallery);
+        if (pickedFile != null) {
+          controller.setUserImage(pickedFile.path);
+        }
+      },
+      child: Text('Change Image'),
+    );
+  }
+}
+
+class SaveButton extends StatelessWidget {
+  final ProfileController controller;
+
+  SaveButton({required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        controller.saveProfile();
+        controller.toggleEditMode();
+      },
+      child: Text('Save User Data'),
+    );
+  }
+}
+
+class ViewProfile extends StatelessWidget {
+  final ProfileController controller;
+  final Authenticate auth;
+
+  ViewProfile({required this.controller, required this.auth});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ProfileImageSection(controller: controller),
+            SizedBox(height: 16),
+            PersonalInfoSection(controller: controller),
+            SizedBox(height: 16),
+            HouseholdsSection(controller: controller),
+            SizedBox(height: 16),
+            SignOutButton(auth: auth),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignOutButton extends StatelessWidget {
+  final Authenticate auth;
+
+  SignOutButton({required this.auth});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        auth.signOut();
+      },
+      child: const Text('Sign Out'),
+    );
+  }
+}
+
+class ShadowedTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+
+  ShadowedTextField({required this.controller, required this.label});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+}
+
+class ShadowedTextBox extends StatelessWidget {
+  final String label;
+  final String value;
+
+  ShadowedTextBox({required this.label, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8.0),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+        borderRadius: BorderRadius.circular(8.0),
+        color: Colors.white,
+      ),
+      padding: EdgeInsets.all(16.0),
+      child: Row(
         children: [
-          ElevatedButton(
-            onPressed: () {
-              auth.signOut();
-            },
-            child: const Text('Sign Out'),
+          Text(
+            '$label: ',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            value,
+            style: TextStyle(fontSize: 18),
           ),
         ],
       ),
